7e25a1d494551452fdb567a9437ec7bad29f00cd
Atef Ben Ali <atef.bettaib@gmail.com>
2020-12-30 14:43:15 +0100

    build: add `typescript` config in `webpack` config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 /*** webpack.config.js ***/
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, "examples/src/index.html"),
-  filename: "./index.html"
+  filename: "./index.html",
 });
+
 module.exports = {
-  entry: path.join(__dirname, "examples/src/index.js"),
+  entry: path.join(__dirname, "examples/src/index.tsx"),
   output: {
     path: path.join(__dirname, "examples/dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [htmlWebpackPlugin],
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".js"],
   },
   devServer: {
-    port: 3001
-  }
+    port: 3001,
+  },
 };
